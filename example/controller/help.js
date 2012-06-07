381c743d1e9b488c6f888359d44f04af90d27360
Lei Zongmin <leizongmin@gmail.com>
2012-06-07 16:12:04 +0800

    增加 e.redirect() 重定向方法

--- example/controller/help.js
+++ example/controller/help.js
@@ -6,4 +6,9 @@ QuickWeb.on('/help', function (e) {
 
 QuickWeb.on('/help', function (e) {
   alert('帮助！');
+  e.redirect('/error?msg=测试一下而已，不必当真~~');
+});
+
+QuickWeb.on('/error', function (e) {
+  e.render('#pop-window-body', 'error', {message: e.query.msg});
 });
